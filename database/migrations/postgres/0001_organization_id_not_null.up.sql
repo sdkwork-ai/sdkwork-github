92941b2a85ed6e6ff321adb8767f58b6d46d9135
zowork <sdkwork@zowalk.com>
2026-08-10 19:02:41 +0800

    chore: sync local changes

--- database/migrations/postgres/0001_organization_id_not_null.up.sql
+++ database/migrations/postgres/0001_organization_id_not_null.up.sql
@@ -18,38 +18,47 @@
 
 BEGIN;
 
+ALTER TABLE github_repository ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_repository SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_repository ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_repository ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_issue ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_issue SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_issue ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_issue ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_plan ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_plan SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_plan ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_plan ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_provider_account ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_provider_account SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_provider_account ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_provider_account ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_oauth_pending ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_oauth_pending SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_oauth_pending ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_oauth_pending ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_tracker_label ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_tracker_label SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_tracker_label ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_tracker_label ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_tracker_milestone ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_tracker_milestone SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_tracker_milestone ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_tracker_milestone ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_tracker_issue ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_tracker_issue SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_tracker_issue ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_tracker_issue ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE github_tracker_roadmap ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE github_tracker_roadmap SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE github_tracker_roadmap ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE github_tracker_roadmap ALTER COLUMN organization_id SET NOT NULL;
